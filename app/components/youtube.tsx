7400c69b0ef8493a86fef1b7b9d176a2e7bff7cd
Kyle's Numb <86553648+KyleDcjnr@users.noreply.github.com>
2026-07-03 13:07:34 +0100

    Update youtube.tsx

--- app/components/youtube.tsx
+++ app/components/youtube.tsx
@@ -1,19 +1,21 @@
 // components/YouTubePlayer.tsx
 import React from 'react';
 import Box from '@chakra-ui/react';
-import YouTube, { Options } from 'react-youtube';
+import YouTube from 'react-youtube';
 
 const YouTubePlayer: React.FC = () => {
   const opts: Options = {
-    height: '500',
-    width: '100%',
-    objectFit: 'contain',
-    playerVars: {
-      autoplay: 0, // set to 1 to autoplay video
-      controls: 1,
-      modestbranding: 0, // Removes YouTube branding
-      rel: 0,
-    },
+   type YouTubeOptions = {
+  height?: string;
+  width?: string;
+  playerVars?: { autoplay?: 0 | 1; [key: string]: any };
+};
+
+const opts: YouTubeOptions = {
+  height: '390',
+  width: '640',
+  playerVars: { autoplay: 1 },
+};
   };
 
   const onReady = (event: { target: any }) => {
